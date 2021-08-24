3539462f91ae34330c66d7c46dd1ca86a647aaef
PainAiko <gitnynjao@gmail.com>
2021-08-24 15:06:07 +0300

    ass

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useEffect} from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import {API_KEY,API_URL} from './src/components/constant';
+
+const {width,heigth} = Dimensions.get('screen');
+const fetchImagesFromPexels = async () => {
+    const data = await fetch(API_URL,{
+      headers: {
+        'Authorization': API_KEY
+      }
+    });
+    const result = await data.json();
+    return result;
+}
+
+
+
 
 export default function App() {
-  return (
+  useEffect(() => {
+    const fetchImages = async () => {
+        const images = await fetchImagesFromPexels()
+        console.log(images);
+    }
+    fetchImages();
+  },[])
+  return (              
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <Text>{API_KEY}</Text>
     </View>
   );
 }
---
+++ src/components/constant.js
@@ -0,0 +1,2 @@
+export const API_KEY ="563492ad6f91700001000001a30ebe7dd43541d192ba4e123be8c06c";
+export const API_URL = "https://api.pexels.com/v1/search?query=nature&orientation=portrait&size=small&per_page=20";
