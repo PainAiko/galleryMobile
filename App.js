e1be15ca8eb249f53be4757b8ad1e27047ef46af
PainAiko <gitnynjao@gmail.com>
2021-08-25 11:24:20 +0300

    add key pexel

--- App.js
+++ App.js
@@ -1,30 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import React,{useEffect} from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import React,{useEffect, useState} from 'react';
+import { StyleSheet, Text, View, Dimensions,SafeAreaView,SafeAreaViewBase,Easing,TouchableOpacity } from 'react-native';
 import {API_KEY,API_URL} from './src/components/constant';
 
 const {width,heigth} = Dimensions.get('screen');
+
 const fetchImagesFromPexels = async () => {
     const data = await fetch(API_URL,{
       headers: {
         'Authorization': API_KEY
       }
     });
-    const result = await data.json();
-    return result;
+    const {photos} = await data.json();
+    return photos;
 }
 
 
 
 
 export default function App() {
+  const [images, setImages] = useState(null)
   useEffect(() => {
     const fetchImages = async () => {
         const images = await fetchImagesFromPexels()
-        console.log(images);
+        setImages(images);
+        //console.log(images);
     }
     fetchImages();
   },[])
+ console.log(images);
+ if (!images) {
+      return(  <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>) 
+  }
   return (              
     <View style={styles.container}>
        <Text>{API_KEY}</Text>
