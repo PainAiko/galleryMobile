e55af20e7d5ab1ae0185fa589b4aec3d2f5ebb3a
PainAiko <gitnynjao@gmail.com>
2021-08-25 12:54:40 +0300

    image slide

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{useEffect, useState} from 'react';
-import { StyleSheet, Text, View, Dimensions,SafeAreaView,SafeAreaViewBase,Easing,TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Dimensions,SafeAreaView,SafeAreaViewBase,Easing,TouchableOpacity, FlatList,Image } from 'react-native';
 import {API_KEY,API_URL} from './src/components/constant';
 
-const {width,heigth} = Dimensions.get('screen');
-
+const {width,height} = Dimensions.get('screen');
+const IMAGE_SIZE = 80;
+const SPACING = 10;
 const fetchImagesFromPexels = async () => {
     const data = await fetch(API_URL,{
       headers: {
@@ -24,11 +25,10 @@ export default function App() {
     const fetchImages = async () => {
         const images = await fetchImagesFromPexels()
         setImages(images);
-        //console.log(images);
     }
     fetchImages();
   },[])
- console.log(images);
+
  if (!images) {
       return(  <View style={styles.container}>
         <Text>Loading...</Text>
@@ -36,7 +36,41 @@ export default function App() {
   }
   return (              
     <View style={styles.container}>
-       <Text>{API_KEY}</Text>
+        <FlatList
+            data={images}
+            horizontal={true}
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+            keyExtractor={item => item.id.toString()}
+            renderItem={({item})=>{
+              return <View style={{width,height}}>
+                 <Image
+                  source={{uri: item.src.portrait}}
+                  style={[StyleSheet.absoluteFillObject]}
+              />
+              </View>
+            }}
+        />
+         <FlatList
+            data={images}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            style={{position: 'absolute',bottom: IMAGE_SIZE}}
+            keyExtractor={item => item.id.toString()}
+            contentContainerStyle={{paddingHorizontal: SPACING}}
+            renderItem={({item})=>{
+              return <Image
+                        style={{
+                          width:IMAGE_SIZE,
+                          height: IMAGE_SIZE, 
+                          borderRadius: 12,
+                          marginRight: SPACING
+                        }}
+                       source={{uri: item.src.portrait}}
+              />
+            }}
+        />
+        <StatusBar backgroundColor='green'/>
     </View>
   );
 }
